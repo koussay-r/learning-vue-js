76e43ff764484465d4c4ca10a80429059c2162f3
koussay-r <koussayr98@gmail.com>
2023-10-05 13:40:13 +0100

    update

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<script setup>
+const handleClick=()=>{
+    alert("you did it")
+}
+</script>
+<template>
+    <div id="navbar" >
+        <p>first vue project</p>
+        <div>
+            <button @click="handleClick" >click me</button>
+        </div>
+    </div>
+</template>
+<style>
+#navbar{
+    display:flex;
+    justify-content:space-between;
+    
+}
+</style>
