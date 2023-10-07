7b0da1772b432331e9a6f2d56021231123aaaada
koussay-r <koussayr98@gmail.com>
2023-10-07 02:09:39 +0100

    update

---
+++ src/components/FirsSlide.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-full absolute h-[570px]">
+    </div>
+    <img :src="bgPic" alt="bgPic" class="w-full absolute h-[570px] object-cover z-[-1] top-0"/>
+</template>
+<script>
+import bgPic from "./../assets/bg9.jpg";
+export default{
+    data(){
+        return {
+            bgPic:bgPic
+        }
+    }
+}
+</script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,51 @@
-<script setup>
-const handleClick=()=>{
-    alert("you did it")
-}
-</script>
 <template>
-    <div id="navbar" >
-        <p>first vue project</p>
-        <div>
-            <button @click="handleClick" >click me</button>
+    <div class="flex p-3 justify-between">
+        <p>Material Kit 2</p>
+        <div class="flex">
+            <div class="flex gap-1">
+                <img :src="pages" class="w-[15px] h-[15px]" alt="images"/>
+                <p>pages</p>
+                <img :src="downArrow" class="w-[10px] h-[10px]" alt="downarrow"/>
+            </div>
+            <div class="flex gap-1">
+                <img :src="sections" class="w-[15px] h-[15px]" alt="sections"/>
+                <p>sections</p>
+                <img :src="downArrow" class="w-[10px] h-[10px]" alt="downarrow"/>
+
+            </div>
+            <div class="flex gap-1">
+                <img :src="docs" class="w-[15px] h-[15px]" alt="docs"/>
+                <p>docs</p>
+                <img :src="downArrow" class="w-[10px] h-[10px]" alt="downarrow"/>
+
+            </div>
+            <div>
+
+
+            </div>
+            <div>
+
+
+            </div>
         </div>
     </div>
 </template>
-<style>
-#navbar{
-    display:flex;
-    justify-content:space-between;
-    
-}
-</style>
+<script>
+import pages from './../assets/icons/layout-fluide.png';
+import sections from './../assets/icons/agrandir-la-fenetre.png';
+import docs from './../assets/icons/document-signe.png';
+import downArrow from './../assets/icons/down-arrow.png';
+import github from './../assets/icons/github.png';
+
+export default {
+  data() {
+    return {
+      pages: pages,
+      sections: sections,
+      docs: docs,
+      downArrow: downArrow,
+      github: github
+    };
+  },
+};
+</script>
